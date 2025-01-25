9855b0933705424544c21fa5fb155a0bf095bb5e
Sam Law <samuel.law@system1group.com>
2025-01-25 10:42:29 +0000

    try fix build

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,8 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req: Request, res: Response, next: NextFunction) {
   res.send('Home page');
 });
 
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,8 +1,8 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req: Request, res: Response, next: NextFunction) {
   res.send('respond with a resource');
 });
 
